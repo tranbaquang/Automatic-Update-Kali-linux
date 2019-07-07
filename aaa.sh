e203c036b5d2954a72fbf1783c156e460e06079c
= <=>
2019-07-07 21:46:22 +0700

    Day len Github

---
+++ aaa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "11111"
+tor.sh
---
+++ code.sh
@@ -0,0 +1,8 @@
+if [ "$UID" -ne 1 ]
+then 
+    echo ">> Hãy chạy quyền root để sử dụng script này !!! "
+    exit
+else
+    echo " ahihi"
+    exit
+fi    
